f08beb49bf178e403e431f9d69eaedfb081cf2f7
bliss administrator <blissadm@egg.esrf.fr>
2020-06-15 16:24:06 +0200

    compression false by default

--- HardwareObjects/queue_model_objects.py
+++ HardwareObjects/queue_model_objects.py
@@ -568,6 +568,7 @@ class DataCollection(TaskNode):
         self.run_offline_processing = True
         self.run_online_processing = False
         self.grid = None
+        self.shape = None
         self.online_processing_results = {"raw": None, "aligned": None}
         self.processing_msg_list = []
         self.workflow_id = None
@@ -1380,7 +1381,7 @@ class PathTemplate(object):
         self.suffix = str()
         self.start_num = int()
         self.num_files = int()
-        self.compression = True
+        self.compression = False
 
         if not hasattr(self, "precision"):
             self.precision = str()
