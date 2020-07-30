451bd35702dbce4a4f0fc689cd492b28751a0298
bliss administrator <blissadm@egg.esrf.fr>
2020-07-30 17:22:49 +0200

    add quick realign.

--- HardwareObjects/ESRF/ID30BBeamCmds.py
+++ HardwareObjects/ESRF/ID30BBeamCmds.py
@@ -1,5 +1,6 @@
 from HardwareRepository.BaseHardwareObjects import HardwareObject
-from .BeamCmds import ControllerCommand, TestCommand
+from HardwareRepository import HardwareRepository as HWR
+from .BeamCmds import ControllerCommand, TestCommand, HWObjActuatorCommand
 
 
 class ID30BBeamCmds(HardwareObject):
@@ -11,10 +12,11 @@ class ID30BBeamCmds(HardwareObject):
         controller.detcover.set_in()
         self.centrebeam = ControllerCommand("Centre beam", controller.centrebeam)
         self.test_cmd = TestCommand("Test command")
-        #self.quick_realign = ControllerCommand(
-        #   "Quick realign", controller.quick_realign
-        #)
+        self.quick_realign = ControllerCommand(
+           "Quick realign", controller.quick_realign
+        )
+        #self.hutchtrigger = HWObjActuatorCommand("Hutchtrigger", HWR.beamline.hutch_interlock)
         #self.anneal = ControllerCommand("Anneal", controller.anneal_procedure)
 
     def get_commands(self):
-        return [self.centrebeam, self.test_cmd]
+        return [self.centrebeam, self.quick_realign, self.test_cmd]
